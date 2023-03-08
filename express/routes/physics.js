4b326df7dc439f656ae3fbe4c45ae333e65a2b12
pupee <aungchanminmarch@gmail.com>
2023-03-08 14:14:39 +0630

    making ready to write lecture note
    I need to be ready chapter 4 lecture notes for my the day after tomorrow class. So I have to write lecture notes first.

--- express/routes/physics.js
+++ express/routes/physics.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+function serveHTML(req, res) {
+  res.sendFile(path.resolve(__dirname, '../views/index.html'));
+}
+router.get('/', serveHTML);
+router.get('/index', serveHTML);
+router.get('/index.html', serveHTML);
+
+router.get('/', function(req, res) {
   res.render('layout', {title: 'IGCSE PHYSICS'});
 });
 
