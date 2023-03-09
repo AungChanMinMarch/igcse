f29f87a944221bc472fc846267569840ab0005b5
pupee <aungchanminmarch@gmail.com>
2023-03-09 19:37:41 +0630

    idk why but that line give me errors

--- express/app.js
+++ express/app.js
@@ -26,7 +26,7 @@ app.use('/physics', physicsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  // next(createError(404));
 });
 
 // error handler
