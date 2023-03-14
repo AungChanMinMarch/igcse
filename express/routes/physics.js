6bc3db1e357e096352306becef3d9145b3733901
pupee <aungchanminmarch@gmail.com>
2023-03-14 14:02:02 +0630

    "/physics/ch",.... are now working

--- express/routes/physics.js
+++ express/routes/physics.js
@@ -2,28 +2,44 @@ var express = require('express');
 var router = express.Router();
 const path = require('path');
 const noteModel = require('../models/note.js');
-
+const choiceQuestionModel = require('../models/choiceQuestion.js');
+const shortQuestionModel = require('../models/shortQuestion.js');
+const practicalQuestionModel = require('../models/practicalQuestion.js');
+function buildSearchObj(paramStr){
+  const [chapter, subChapter, number] = paramStr.split('.');
+    let searchObj = { chapter : Number.parseInt(chapter)};
+    if(!!subChapter){
+      searchObj.subChapter = Number.parseInt(subChapter);
+    }
+    if(!!number){
+      searchObj.number= Number.parseInt(number)
+    }
+    return searchObj
+}
+function responer(model, hasSearchObj){
+  return function(req, res){
+    const searchObj = hasSearchObj ? buildSearchObj(req.params.id) : {}
+    model.find(searchObj).then(function(documents){
+      res.render('index', {title: 'IGCSE PHYSICS', documents: documents});
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+}
 router.get('/', function(req, res) {
   res.render('index', {title: 'IGCSE PHYSICS', documents: []});
 });
 
-router.get('/ch/:id', function(req, res){
-  const [chapter, subChapter, number] = req.params.id.split('.');
-  let searchObj = {};
-  if(!!chapter){
-    searchObj.chapter = Number.parseInt(chapter);
-  }
-  if(!!subChapter){
-    searchObj.subChapter = Number.parseInt(subChapter);
-  }
-  if(!!number){
-    searchObj.number= Number.parseInt(number)
-  }
-  noteModel.find(searchObj).then(function(documents){
-    res.render('index', {title: 'IGCSE PHYSICS', documents: documents});
-  }).catch(function(err) {
-    console.log(err);
-  });
-})
+router.get('/ch', responer(noteModel, false));
+router.get('/ch/:id', responer(noteModel, true));
+
+router.get('/cq', responer(choiceQuestionModel, false));
+router.get('/cq/:id', responer(choiceQuestionModel, true));
+
+router.get('/sq', responer(shortQuestionModel, false));
+router.get('/sq/:id', responer(shortQuestionModel, true));
+
+router.get('/pq', responer(practicalQuestionModel, false));
+router.get('/pq/:id', responer(practicalQuestionModel, true));
 
 module.exports = router;
