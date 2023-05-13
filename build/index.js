8b7598c208d3d46b941551c53b06b100ff683092
AungChanMinMarch <77224022+AungChanMinMarch@users.noreply.github.com>
2023-05-13 19:36:30 +0630

    Update index.js

--- build/index.js
+++ build/index.js
@@ -3,6 +3,6 @@ const log = console.log;
 const mongoose = require('mongoose');
 const uri = process.argv[2]; // slice the first two elements which are not arguments
 log(uri.startsWith("mongodb"))
-// mongoose.connect(uri).then(()=>{
-//   log("connection success")
-// })
+mongoose.connect(uri).then(()=>{
+  log("connection success")
+})
