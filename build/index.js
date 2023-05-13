e9a113096d081934358dc2c5f6bcdb38ffcbe35e
AungChanMinMarch <77224022+AungChanMinMarch@users.noreply.github.com>
2023-05-13 16:39:55 +0630

    fix mongoose not found 2

--- build/index.js
+++ build/index.js
@@ -1,5 +1,6 @@
 const log = console.log;
 
+const mongoose = require('mongoose');
 const uri = process.argv[2]; // slice the first two elements which are not arguments
 
 mongoose.connect(uri).then(()=>{
